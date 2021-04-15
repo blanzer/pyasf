89f5d440d66b5d064f2733fceaff558dfe76faf1
Gabriel Kreuzer S <kreuzers@tutanota.com>
2021-04-15 13:56:29 +0200

    split utils & api. added datalogger and publisher

--- pyasf/pyasf.py
+++ pyasf/pyasf.py
@@ -1,21 +1,8 @@
 import zmq
 import json
 from loguru import logger
-
-
-class _NpEncoder(json.JSONEncoder):
-    """Object to serialize our dicts into json file"""
-    def default(self, obj):
-        if isinstance(obj, np.integer):
-            return int(obj)
-        elif isinstance(obj, np.floating):
-            return float(obj)
-        elif isinstance(obj, np.dtype):
-            return str(obj)  
-        elif isinstance(obj, np.ndarray):
-            return obj.tolist()
-        else:
-            return super(NpEncoder, self).default(obj)   
+import pathlib
+import utils
 
 
 class ControllerLink():
@@ -64,7 +51,7 @@ class ControllerLink():
         Returns:
             bool:  message was received sucessfully
         """
-        self.req_socket.send_json(json.dumps(data, cls=_NpEncoder))
+        self.req_socket.send_json(json.dumps(data, cls=utils.NpEncoder))
         try:
             reply = self.req_socket.recv()
         except zmq.error.Again as ea:
@@ -98,3 +85,130 @@ class ControllerLink():
         all_panels =  {"panel_orientation": [ axis for panel in range(30) for axis in [azimuth, altitude] ]}
         self.communicate(all_panels)
         
+
+class Datalogger:
+    """ This a a zmq subscription datalogger. Takes everything and writes it to file.
+    Can record by calling:
+        datalogger = zmqhandler.ZmqDataLogger(ip=...,
+                                          filename=...,
+                                          save_to_file=...)
+  
+        data = datalogger.receive(lambda_gen=lambda x: "row_4" in x[0])
+
+    """    
+    def __init__(self, ip="192.168.66.213", filename="unnamed-experiment", save_to_file=True):
+        self.context = zmq.Context()
+        logger.info(f"Connecting to Publisher as Subscriber on {ip}:9872")
+        self._setup_connection(ip)
+        self.save_to_file = save_to_file
+        self._file = None
+        self._flush_time = time.perf_counter()
+
+        if save_to_file:
+            self.filename =  filename + datetime.datetime.now().strftime("-%Y-%m-%d_%H-%M-%S") + ".csv"
+            path_file = pathlib.Path(self.filename)
+            if not path_file.parent.exists():
+                path_file.parent.mkdir(parents=True)
+            self._setup_csv_file()
+
+        self.header_written = False
+        self.data = []
+
+    def _setup_connection(self, ip):
+        self.sub_socket = self.context.socket(zmq.SUB)
+        self.sub_socket.connect(f"tcp://{ip}:9872")
+        self.sub_socket.subscribe("")
+        # self.req_socket.setsockopt(zmq.SUBSCRIBE)
+
+    def _setup_csv_file(self, rotation=""):
+        logger.info(f"Writing File: {self.filename+rotation}")
+        if rotation != "":
+            rotation = "." + rotation
+
+        self._file =  open(self.filename+rotation, "w")
+    
+    def get_header(self, json_data):
+        to_save = []
+        flat_dict = utils.dict_flatten(json_data)
+
+        for key, value in flat_dict.items():
+            to_save.append(key)
+
+        return to_save
+
+    def get_data(self, dict_data):
+        to_save = []
+        to_data = []
+
+        flat_dict = utils.dict_flatten(dict_data)
+
+        for key, value in flat_dict.items():
+            to_save.append(str(value))
+            to_data.append(value)
+
+        self.data = np.array(to_data[1:])
+
+        return to_save
+
+    def _write_header(self, dict_data):
+        to_save = self.get_header(dict_data)
+        self._write(",".join(to_save)+"\n")
+
+    def receive(self, lambda_gen = None):
+        raw_data = self.sub_socket.recv_json()
+
+        deserialized_dict = utils.NpTypeDecoder(raw_data)
+  
+        data_arrays = {}
+
+        if lambda_gen is not None:
+
+            data_dict = { "timestamp": deserialized_dict["timestamp"],
+                          **dict(filter(lambda_gen, deserialized_dict.items()))
+                        }
+
+        else:
+            data_dict = deserialized_dict
+
+        if not self.header_written and self.save_to_file:
+            logger.info("Header written.")
+            self._write_header(data_dict)
+            self.header_written = True
+
+        to_save = self.get_data(data_dict)
+
+        if self.save_to_file:
+            self._write(",".join(to_save)+"\n")
+        
+        return data_arrays
+
+    def _write(self, data):
+        self._file.write(data)
+
+        current_time = time.perf_counter()
+
+        if current_time - self._flush_time > 1:
+            self._file.flush()
+            self._flush_time = current_time
+        
+    def close(self):
+        if self._file is not None:
+            self._file.close()
+
+
+class Publisher():
+    def __init__(self, port=9872, pass_type=False):
+        logger.debug("Init ZmqBasePublisher")
+        self.pass_type = pass_type
+        self.context = zmq.Context()
+        self.skt = self.context.socket(zmq.PUB)
+        self.skt.bind(f"tcp://*:{port}")
+
+    def publish(self, data_dict):
+        if self.pass_type:
+            dtyped_values = utils.NpTypeEncoder(data)
+            self.skt.send_json( json.dumps(dtyped_values, cls=utils.NpEncoder) )
+        else:
+            data = utils.dict_flatten(data_dict)
+            self.skt.send_json( json.dumps(data, cls=utils.NpEncoder) )
+
---
+++ pyasf/utils.py
@@ -0,0 +1,87 @@
+import zmq
+import json
+from loguru import logger
+
+
+def dict_flatten(in_dict, dict_out=None, parent_key=None, separator="_"):
+    """flattens dict to serialize"""
+    if dict_out is None:
+        dict_out = {}
+
+    for k, v in in_dict.items():
+        k = f"{parent_key}{separator}{k}" if parent_key else k
+        if isinstance(v, dict):
+            dict_flatten(in_dict=v, dict_out=dict_out, parent_key=k)
+            continue
+
+        elif array_flatten(v, dict_out, k):
+            continue
+
+        dict_out[k] = v
+
+    return dict_out
+
+def array_flatten(unflatten, in_dict, dict_key):
+
+    if isinstance(unflatten, np.ndarray):
+        unflatten = unflatten.flatten().tolist()
+
+    if isinstance(unflatten, list):
+        for _it, _value in enumerate(unflatten):
+            if isinstance(_value, list):
+                array_flatten(_value, in_dict, f"{dict_key}_{_it}")
+            else:
+                in_dict[f"{dict_key}_{_it}"] = _value
+        return 1
+    
+    return 0
+
+
+class NpEncoder(json.JSONEncoder):
+    """Object to serialize our dicts into json file"""
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.dtype):
+            return str(obj)  
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(NpEncoder, self).default(obj)   
+
+
+def NpTypeEncoder(in_dict, out_dict=None, parent_key=None, separator="/"):
+    """Stores numpy dtype information in dicts to be serialized"""
+    if out_dict is None:
+        out_dict = {}
+
+    for k, v in in_dict.items():
+        k = f"{parent_key}{separator}{k}" if parent_key else k
+
+        if isinstance(v, dict):
+            NpTypeEncoder(in_dict=v, out_dict=out_dict, parent_key=k)
+            continue
+
+        if isinstance(v, np.ndarray):
+            out_dict[f"{k}_dtype"]= v.dtype
+
+        out_dict[k] = v
+        
+    return out_dict
+
+    
+def NpTypeDecoder(json_loads):
+    """Recreates dict with received numpy dtypes."""
+    out_dict = {}
+
+    for k, v in json_loads.items():
+        if "_dtype" in k:
+            out_dict[k[:-6]] = v
+        elif k in out_dict.keys():
+            out_dict[k] = np.array(v, dtype=out_dict[k])
+        else:
+            out_dict[k] = v
+
+    return out_dict
